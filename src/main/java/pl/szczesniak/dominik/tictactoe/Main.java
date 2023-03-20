30e30fdc3cc29e3ec016ce5c1b69046fa9d7a500
Dominik Szczęśniak <dominikszczesniak128@gmail.com>
2023-03-20 17:34:29 +0100

    first commit

---
+++ src/main/java/pl/szczesniak/dominik/tictactoe/Main.java
@@ -0,0 +1,9 @@
+package pl.szczesniak.dominik.tictactoe;
+
+import pl.szczesniak.dominik.tictactoe.consoletictactoeapp.game.application.TicTacToeConsoleApp;
+
+public class Main {
+	public static void main(String[] args) {
+		new TicTacToeConsoleApp().run();
+	}
+}
